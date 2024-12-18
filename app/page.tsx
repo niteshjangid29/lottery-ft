bdb267518ebbe5efbde355f5267e6e8b9e2407a6
niteshjangid29 <nitesh_j@ee.iitr.ac.in>
2024-12-18 19:41:01 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="bg-primary text-secondary">
+      <h1 className="text-4xl">Hello, world!</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
